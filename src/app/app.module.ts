b5f3662e3c1d73a0b1dabea4cb11d82c0dbb815b
Akshay Borana <akshayborana1@gmail.com>
2019-04-28 00:22:28 -0400

    creating store-detail component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreDetailModule } from "./store-detail/store-detail.module";
 
 import { AppComponent } from './app.component';
 import { SharedModule } from "./shared/shared.module";
@@ -10,7 +11,8 @@ import { SharedModule } from "./shared/shared.module";
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    StoreDetailModule
   ],
   providers: [],
   bootstrap: [AppComponent]
